554a5f9fc1b065922d608cdd6e10572219bef2f0
Tracks12 <cardinal.florian.erwan@gmail.com>
2021-11-04 00:51:14 +0100

    add login/register users in database with manager & add middleware

---
+++ common/constants.js
@@ -0,0 +1,21 @@
+/**
+ * Project : CookAppAPI
+ * Name    : constants.js
+ * Date    : 27/10/2021
+ * Autor   : CARDINAL Florian
+ */
+
+// Constants of remote MongoDB DataBase Access
+export const DB_URL = "...";
+export const DB_NAME = "cook-app-db";
+
+// Constants of express server listening
+export const EXPRESS_IP = "localhost";
+export const EXPRESS_PORT = 5000;
+
+// Constants of token
+export const TOKEN_SECRET = "...";
+
+/**
+ * END
+ */
